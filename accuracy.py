fc2aa52495a97d16d9a49a7c2a181030b6467f47
Teresa Ferreira <82399829+mteresapcrf@users.noreply.github.com>
2024-06-08 21:06:34 +0200

    update, calibration percentile is the most updated

--- accuracy.py
+++ accuracy.py
@@ -3,128 +3,104 @@ import random
 import quantisations as qt
 import basics as bs
 
+quantization_functions = {
+    "scalar_quantisation_max": scalar_quantisation_max,
+    "scalar_quantisation_percentile": scalar_quantisation_percentile,
+    "scalar_quantisation_tensorrt": scalar_quantisation_tensorrt
+}
+
 
 # Function to compare accuracies
-def compare_accuracies_euc(pairs, m=1000):
-    # Initialize counters
-    c_t = w_t = c_s = w_s = 0
-    c = w = c_t_s = w_t_s = c_s_s = w_s_s = 0
-    c_ss = w_ss = 0
+# Function to compare accuracies
+def compare_accuracies_euc(pairs, m=1000, quant_funcs=None):
+    if quant_funcs is None:
+        quant_funcs = quantization_functions
 
-    fn_threshold=10
-    sf_threshold=10.734  
+    # Initialize counters
+    counters = {name: {'c_t': 0, 'w_t': 0, 'c_s': 0, 'w_s': 0, 'c': 0, 'w': 0, 'c_t_s': 0, 'w_t_s': 0, 'c_s_s': 0, 'w_s_s': 0, 'c_ss': 0, 'w_ss': 0} for name in quant_funcs.keys()}
+
+    fn_threshold = 10
+    sf_threshold = 10.734  
 
     for i in range(m):
-        # print(i)
-        a, b, imga, imgb = pairs[i]
-        
+        a, b, imga, imgb, n = pairs[i]
+
         # Facenet calculations
         a_q_tensor = qt.quantize_tensor(a)
         b_q_tensor = qt.quantize_tensor(b)
         a_qn_t = a_q_tensor / np.linalg.norm(a_q_tensor)
         b_qn_t = b_q_tensor / np.linalg.norm(b_q_tensor)
 
-        a_quant = qt.scalar_quantisation_percentile(a)
-        b_quant = qt.scalar_quantisation_percentile(b)
-        a_qn_s = a_quant / np.linalg.norm(a_quant)
-        b_qn_s = b_quant / np.linalg.norm(b_quant)
+        for name, quant_func in quant_funcs.items():
+            a_quant = quant_func(a)
+            b_quant = quant_func(b)
+            a_qn_s = a_quant / np.linalg.norm(a_quant)
+            b_qn_s = b_quant / np.linalg.norm(b_quant)
 
-        a_n = a / np.linalg.norm(a)
-        b_n = b / np.linalg.norm(b)
+            a_n = a / np.linalg.norm(a)
+            b_n = b / np.linalg.norm(b)
 
-        # SFace calculations
-        a_s = bs.get_embedding(imga)
-        b_s = bs.get_embedding(imgb)
+            # SFace calculations
+            a_s = bs.get_embedding(imga)
+            b_s = bs.get_embedding(imgb)
 
-        a_q_tensor_s = qt.quantize_tensor(a_s)
-        b_q_tensor_s = qt.quantize_tensor(b_s)
-        a_qn_t_s = a_q_tensor_s / np.linalg.norm(a_q_tensor_s)
-        b_qn_t_s = b_q_tensor_s / np.linalg.norm(b_q_tensor_s)
+            a_q_tensor_s = qt.quantize_tensor(a_s)
+            b_q_tensor_s = qt.quantize_tensor(b_s)
+            a_qn_t_s = a_q_tensor_s / np.linalg.norm(a_q_tensor_s)
+            b_qn_t_s = b_q_tensor_s / np.linalg.norm(b_q_tensor_s)
 
-        a_quant_s = qt.scalar_quantisation_percentile(a_s)
-        b_quant_s = qt.scalar_quantisation_percentile(b_s)
-        a_qn_s_s = a_quant_s / np.linalg.norm(a_quant_s)
-        b_qn_s_s = b_quant_s / np.linalg.norm(b_quant_s)
+            a_quant_s = quant_func(a_s)
+            b_quant_s = quant_func(b_s)
+            a_qn_s_s = a_quant_s / np.linalg.norm(a_quant_s)
+            b_qn_s_s = b_quant_s / np.linalg.norm(b_quant_s)
 
-        a_n_s = a_s / np.linalg.norm(a_s)
-        b_n_s = b_s / np.linalg.norm(b_s)
+            a_n_s = a_s / np.linalg.norm(a_s)
+            b_n_s = b_s / np.linalg.norm(b_s)
 
-        # Facenet comparison
-        if n:
-            if bs.euclidean_distance(a_qn_t, b_qn_t) > fn_threshold:
-                w_t += 1
-            else:
-                c_t += 1
-            if bs.euclidean_distance(a_qn_s, b_qn_s) > fn_threshold:
-                w_s += 1
-            else:
-                c_s += 1
-            if bs.euclidean_distance(a_n, b_n) > fn_threshold:
-                w += 1
-            else:
-                c += 1
-        else:
-            if bs.euclidean_distance(a_qn_t, b_qn_t) > fn_threshold:
-                c_t += 1
-            else:
-                w_t += 1
-            if bs.euclidean_distance(a_qn_s, b_qn_s) > fn_threshold:
-                c_s += 1
-            else:
-                w_s += 1
-            if bs.euclidean_distance(a_n, b_n) > fn_threshold:
-                c += 1
-            else:
-                w += 1
+            # Facenet comparison
+            if n:
+                if bs.euclidean_distance(a_qn_t, b_qn_t) > fn_threshold:
+                    counters[name]['w_t'] += 1
+                else:
+                    counters[name]['c_t'] += 1
+                if bs.euclidean_distance(a_qn_s, b_qn_s) > fn_threshold:
+                    counters[name]['w_s'] += 1
+                else:
+                    counters[name]['c_s'] += 1
+                if bs.euclidean_distance(a_n, b_n) > fn_threshold:
+                    counters[name]['w'] += 1
+                else:
+                    counters[name]['c'] += 1
 
-        # SFace comparison
-        if n:
+            # SFace comparison
             if bs.euclidean_distance(a_qn_t_s, b_qn_t_s) > sf_threshold:
-                w_t_s += 1
+                counters[name]['w_t_s'] += 1
             else:
-                c_t_s += 1
+                counters[name]['c_t_s'] += 1
             if bs.euclidean_distance(a_qn_s_s, b_qn_s_s) > sf_threshold:
-                w_s_s += 1
+                counters[name]['w_s_s'] += 1
             else:
-                c_s_s += 1
+                counters[name]['c_s_s'] += 1
             if bs.euclidean_distance(a_n_s, b_n_s) > sf_threshold:
-                w_ss += 1
+                counters[name]['w_ss'] += 1
             else:
-                c_ss += 1
-        else:
-            if bs.euclidean_distance(a_qn_t_s, b_qn_t_s) > sf_threshold:
-                c_t_s += 1
-            else:
-                w_t_s += 1
-            if bs.euclidean_distance(a_qn_s_s, b_qn_s_s) > sf_threshold:
-                c_s_s += 1
-            else:
-                w_s_s += 1
-            if bs.euclidean_distance(a_n_s, b_n_s) > sf_threshold:
-                c_ss += 1
-            else:
-                w_ss += 1
+                counters[name]['c_ss'] += 1
 
-    correct_f_c = [c, c_s, c_t]
-    incorrect_f_c = [w, w_s, w_t]
-    correct_c = [c_ss, c_s_s, c_t_s]
-    incorrect_c = [w_ss, w_s_s, w_t_s]
-
-    return correct_f_c, incorrect_f_c, correct_c, incorrect_c
+    return counters
 
 # Function to compare accuracies
-def compare_accuracies_cos(pairs, m=1000):
+def compare_accuracies_cos(pairs, m=1000, quant_funcs=None):
+    if quant_funcs is None:
+        quant_funcs = quantization_functions
+
     # Initialize counters
-    c_t = w_t = c_s = w_s = 0
-    c = w = c_t_s = w_t_s = c_s_s = w_s_s = 0
-    c_ss = w_ss = 0
+    counters = {name: {'c_t': 0, 'w_t': 0, 'c_s': 0, 'w_s': 0, 'c': 0, 'w': 0, 'c_t_s': 0, 'w_t_s': 0, 'c_s_s': 0, 'w_s_s': 0, 'c_ss': 0, 'w_ss': 0} for name in quant_funcs.keys()}
 
     fn_threshold_cos = 0.4
     sf_threshold_cos = 0.593
 
     for i in range(m):
-        # print(i)
-        a, b, imga, imgb = pairs[i]
+        a, b, imga, imgb, n = pairs[i]
         
         # Facenet calculations
         a_q_tensor = qt.quantize_tensor(a)
@@ -132,90 +108,86 @@ def compare_accuracies_cos(pairs, m=1000):
         a_qn_t = a_q_tensor / np.linalg.norm(a_q_tensor)
         b_qn_t = b_q_tensor / np.linalg.norm(b_q_tensor)
 
-        a_quant = qt.scalar_quantisation_percentile(a)
-        b_quant = qt.scalar_quantisation_percentile(b)
-        a_qn_s = a_quant / np.linalg.norm(a_quant)
-        b_qn_s = b_quant / np.linalg.norm(b_quant)
+        for name, quant_func in quant_funcs.items():
+            a_quant = quant_func(a)
+            b_quant = quant_func(b)
+            a_qn_s = a_quant / np.linalg.norm(a_quant)
+            b_qn_s = b_quant / np.linalg.norm(b_quant)
 
-        a_n = a / np.linalg.norm(a)
-        b_n = b / np.linalg.norm(b)
+            a_n = a / np.linalg.norm(a)
+            b_n = b / np.linalg.norm(b)
 
-        # SFace calculations
-        a_s = bs.get_embedding(imga)
-        b_s = bs.get_embedding(imgb)
+            # SFace calculations
+            a_s = bs.get_embedding(imga)
+            b_s = bs.get_embedding(imgb)
 
-        a_q_tensor_s = qt.quantize_tensor(a_s)
-        b_q_tensor_s = qt.quantize_tensor(b_s)
-        a_qn_t_s = a_q_tensor_s / np.linalg.norm(a_q_tensor_s)
-        b_qn_t_s = b_q_tensor_s / np.linalg.norm(b_q_tensor_s)
+            a_q_tensor_s = qt.quantize_tensor(a_s)
+            b_q_tensor_s = qt.quantize_tensor(b_s)
+            a_qn_t_s = a_q_tensor_s / np.linalg.norm(a_q_tensor_s)
+            b_qn_t_s = b_q_tensor_s / np.linalg.norm(b_q_tensor_s)
 
-        a_quant_s = qt.scalar_quantisation_percentile(a_s)
-        b_quant_s = qt.scalar_quantisation_percentile(b_s)
-        a_qn_s_s = a_quant_s / np.linalg.norm(a_quant_s)
-        b_qn_s_s = b_quant_s / np.linalg.norm(b_quant_s)
+            a_quant_s = quant_func(a_s)
+            b_quant_s = quant_func(b_s)
+            a_qn_s_s = a_quant_s / np.linalg.norm(a_quant_s)
+            b_qn_s_s = b_quant_s / np.linalg.norm(b_quant_s)
 
-        a_n_s = a_s / np.linalg.norm(a_s)
-        b_n_s = b_s / np.linalg.norm(b_s)
+            a_n_s = a_s / np.linalg.norm(a_s)
+            b_n_s = b_s / np.linalg.norm(b_s)
 
-        # Facenet comparison
-        if n:
-            if bs.get_cos_dist_numpy(a_qn_t, b_qn_t) > fn_threshold_cos:
-                w_t += 1
+            # Facenet comparison
+            if n:
+                if bs.get_cos_dist_numpy(a_qn_t, b_qn_t) > fn_threshold_cos:
+                    counters[name]['w_t'] += 1
+                else:
+                    counters[name]['c_t'] += 1
+                if bs.get_cos_dist_numpy(a_qn_s, b_qn_s) > fn_threshold_cos:
+                    counters[name]['w_s'] += 1
+                else:
+                    counters[name]['c_s'] += 1
+                if bs.get_cos_dist_numpy(a_n, b_n) > fn_threshold_cos:
+                    counters[name]['w'] += 1
+                else:
+                    counters[name]['c'] += 1
             else:
-                c_t += 1
-            if bs.get_cos_dist_numpy(a_qn_s, b_qn_s) > fn_threshold_cos:
-                w_s += 1
-            else:
-                c_s += 1
-            if bs.get_cos_dist_numpy(a_n, b_n) > fn_threshold_cos:
-                w += 1
-            else:
-                c += 1
-        else:
-            if bs.get_cos_dist_numpy(a_qn_t, b_qn_t) > fn_threshold_cos:
-                c_t += 1
-            else:
-                w_t += 1
-            if bs.get_cos_dist_numpy(a_qn_s, b_qn_s) > fn_threshold_cos:
-                c_s += 1
-            else:
-                w_s += 1
-            if bs.get_cos_dist_numpy(a_n, b_n) > fn_threshold_cos:
-                c += 1
-            else:
-                w += 1
+                if bs.get_cos_dist_numpy(a_qn_t, b_qn_t) > fn_threshold_cos:
+                    counters[name]['c_t'] += 1
+                else:
+                    counters[name]['w_t'] += 1
+                if bs.get_cos_dist_numpy(a_qn_s, b_qn_s) > fn_threshold_cos:
+                    counters[name]['c_s'] += 1
+                else:
+                    counters[name]['w_s'] += 1
+                if bs.get_cos_dist_numpy(a_n, b_n) > fn_threshold_cos:
+                    counters[name]['c'] += 1
+                else:
+                    counters[name]['w'] += 1
 
-        # SFace comparison
-        if n:
-            if bs.get_cos_dist_numpy(a_qn_t_s, b_qn_t_s) > sf_threshold_cos:
-                w_t_s += 1
+            # SFace comparison
+            if n:
+                if bs.get_cos_dist_numpy(a_qn_t_s, b_qn_t_s) > sf_threshold_cos:
+                    counters[name]['w_t_s'] += 1
+                else:
+                    counters[name]['c_t_s'] += 1
+                if bs.get_cos_dist_numpy(a_qn_s_s, b_qn_s_s) > sf_threshold_cos:
+                    counters[name]['w_s_s'] += 1
+                else:
+                    counters[name]['c_s_s'] += 1
+                if bs.get_cos_dist_numpy(a_n_s, b_n_s) > sf_threshold_cos:
+                    counters[name]['w_ss'] += 1
+                else:
+                    counters[name]['c_ss'] += 1
             else:
-                c_t_s += 1
-            if bs.get_cos_dist_numpy(a_qn_s_s, b_qn_s_s) > sf_threshold_cos:
-                w_s_s += 1
-            else:
-                c_s_s += 1
-            if bs.get_cos_dist_numpy(a_n_s, b_n_s) > sf_threshold_cos:
-                w_ss += 1
-            else:
-                c_ss += 1
-        else:
-            if bs.get_cos_dist_numpy(a_qn_t_s, b_qn_t_s) > sf_threshold_cos:
-                c_t_s += 1
-            else:
-                w_t_s += 1
-            if bs.get_cos_dist_numpy(a_qn_s_s, b_qn_s_s) > sf_threshold_cos:
-                c_s_s += 1
-            else:
-                w_s_s += 1
-            if bs.get_cos_dist_numpy(a_n_s, b_n_s) > sf_threshold_cos:
-                c_ss += 1
-            else:
-                w_ss += 1
+                if bs.get_cos_dist_numpy(a_qn_t_s, b_qn_t_s) > sf_threshold_cos:
+                    counters[name]['c_t_s'] += 1
+                else:
+                    counters[name]['w_t_s'] += 1
+                if bs.get_cos_dist_numpy(a_qn_s_s, b_qn_s_s) > sf_threshold_cos:
+                    counters[name]['c_s_s'] += 1
+                else:
+                    counters[name]['w_s_s'] += 1
+                if bs.get_cos_dist_numpy(a_n_s, b_n_s) > sf_threshold_cos:
+                    counters[name]['c_ss'] += 1
+                else:
+                    counters[name]['w_ss'] += 1
 
-    correct_f_c = [c, c_s, c_t]
-    incorrect_f_c = [w, w_s, w_t]
-    correct_c = [c_ss, c_s_s, c_t_s]
-    incorrect_c = [w_ss, w_s_s, w_t_s]
-
-    return correct_f_c, incorrect_f_c, correct_c, incorrect_c
+    return counters
--- quantisations.py
+++ quantisations.py
@@ -33,6 +33,110 @@ def scalar_quantisation_percentile(values, lower_bound=None, upper_bound=None):
     
     return quantized_values
 
+def max_calibration(embedding):
+    """
+    Calibrate the range of values in the embedding using the maximum absolute value method.
+    """
+    max_abs_val = np.max(np.abs(embedding))
+    return -max_abs_val, max_abs_val
+
+def scalar_quantisation_max(values, lower_bound=None, upper_bound=None):
+    """
+    Quantize the values to int8 using the calibrated range.
+    """
+    # Ensure the input is a numpy array
+    values = np.array(values, dtype=np.float32)
+    
+    # If no calibration bounds are provided, calculate them using max calibration
+    if lower_bound is None or upper_bound is None:
+        lower_bound, upper_bound = max_calibration(values)
+    
+    # Calculate the scale factor and zero point
+    qmin = -128
+    qmax = 127
+    scale = (upper_bound - lower_bound) / (qmax - qmin)
+    zero_point = qmin - round(lower_bound / scale)
+    
+    # Quantize the values
+    quantized_values = np.clip(np.round(values / scale + zero_point), qmin, qmax).astype(np.int8)
+    
+    return quantized_values
+
+
+
+def calculate_kl_divergence(P, Q):
+    """
+    Calculate the KL divergence between distributions P and Q.
+    """
+    P = np.array(P, dtype=np.float32)
+    Q = np.array(Q, dtype=np.float32)
+    P = P / np.sum(P)
+    Q = Q / np.sum(Q)
+    kl_div = np.sum(np.where(P != 0, P * np.log(P / Q), 0))
+    return kl_div
+
+def calibrate_using_kl_divergence(hist, num_bins=2048, num_quantized_bins=128):
+    """
+    Calibrate the quantization range using KL-divergence method.
+    """
+    histogram = np.array(hist, dtype=np.float32)
+    total_bins = len(histogram)
+    thresholds = []
+
+    for i in range(num_quantized_bins, total_bins):
+        P = histogram[:i].copy()
+        P[i-1] += np.sum(histogram[i:])
+        Q = np.zeros(i, dtype=np.float32)
+        
+        step = i / num_quantized_bins
+        for j in range(num_quantized_bins):
+            start = int(j * step)
+            end = min(int((j + 1) * step), i)
+            Q[start:end] += np.sum(P[start:end])
+        
+        Q = np.interp(np.arange(i), np.arange(0, i, step), Q[:int(i/step)])
+        kl_div = calculate_kl_divergence(P, Q)
+        thresholds.append((kl_div, i))
+
+    thresholds.sort(key=lambda x: x[0])
+    optimal_threshold = thresholds[0][1]
+    return optimal_threshold
+
+def collect_histogram(values, num_bins=2048):
+    """
+    Collect the histogram of the given values.
+    """
+    min_val = np.min(values)
+    max_val = np.max(values)
+    histogram, bin_edges = np.histogram(values, bins=num_bins, range=(min_val, max_val))
+    return histogram, min_val, max_val
+
+def scalar_quantisation_tensorrt(values, histogram=None, num_bins=2048):
+    """
+    Quantize the values to int8 using KL-divergence calibration method.
+    """
+    # Ensure the input is a numpy array
+    values = np.array(values, dtype=np.float32)
+    
+    # Collect histogram if not provided
+    if histogram is None:
+        histogram, min_val, max_val = collect_histogram(values, num_bins)
+    else:
+        min_val, max_val = np.min(values), np.max(values)
+    
+    # Perform KL-divergence calibration to find the optimal threshold
+    optimal_threshold_bin = calibrate_using_kl_divergence(histogram, num_bins)
+    
+    # Calculate the scale factor and zero point
+    threshold_value = min_val + (max_val - min_val) * (optimal_threshold_bin / num_bins)
+    scale = threshold_value / 127.0
+    zero_point = 0
+    
+    # Quantize the values
+    quantized_values = np.clip(np.round(values / scale), -128, 127).astype(np.int8)
+    
+    return quantized_values
+
 def quantize_tensor(input_tensor, T=tf.qint8, mode='MIN_COMBINED', round_mode='HALF_AWAY_FROM_ZERO', name=None, narrow_range=False, axis=None, ensure_minimum_range=0.01):
     quantized_tensor, _, _ = tf.quantization.quantize(
         input=input_tensor,
